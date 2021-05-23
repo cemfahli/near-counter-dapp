45c8e79027028254d3d33e3d119ce87abea54391
Cem Fahliogullari <cemfahli@gmail.com>
2021-05-22 22:58:22 -0400

    Add conditional text to description

--- src/App.svelte
+++ src/App.svelte
@@ -73,7 +73,12 @@
 	<div class="text-center flex justify-evenly items-center flex-col flex-grow-0 md:flex-row p-4 md:p-9 h-full">
 		<div class="p-4 md:px-4">
 			<h1 class="font-bold text-xl pb-6">Hello {(wallet && wallet.getAccountId()) || 'Friend'}!</h1>
-			This is a simple Counter dapp running on the NEAR testnet. <br> Sign in to your Near account to increase or decrease the count!
+			This is a simple counter dapp running on the NEAR testnet. 
+			{#if !wallet || !wallet.isSignedIn()}
+				<br> Sign in to your Near account to increase or decrease the count!
+			{:else}
+				<br> You can increase or decrease the count with some gas!
+			{/if}
 		</div>
 
 		<div class="w-3/4 md:w-auto">
